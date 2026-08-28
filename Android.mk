5062dd44dfb02a888e2e70e8fd9318cce02cafb8
pnguyen879 <howardson2001@gmail.com>
2026-08-28 17:51:05 -0500

    timelm: Extract proprietary firmware
    Use engineered abl.img while at it in order to access the bootloader from recovery mode, using the original stock one restricts the user to reboot to bootloader

--- Android.mk
+++ Android.mk
@@ -3,3 +3,27 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),timelm)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,51c8e464a269ef629d39c9662420af39203aab49)
+$(call add-radio-file-sha1-checked,radio/akmu.img,6d6ec9f2dac648b530018f5cac9e3ea4b75c44f1)
+$(call add-radio-file-sha1-checked,radio/aop.img,74200723b1796cae8e2060ee9e565c3c5632bb23)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,039980388f29f9399329133bc22374b7a5ab6b99)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,bd0ac1e181fefed59bdbf100e0ca22fdbf3ab716)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,fe8aafcf885c3b072ceadb9b8299dbf95511c8ca)
+$(call add-radio-file-sha1-checked,radio/dsp.img,c236be9ed75adc255b9e6ba19ee45ae91b81e03e)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,99d8d14f4f71b34d374a568e7325ef8eae5c4018)
+$(call add-radio-file-sha1-checked,radio/hyp.img,c9fe6c99c31d17beec0ab1b7423505beb4cfb255)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,9e9ca8198ab67fad6a0abe421ff77a0a2fd8dd7b)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,8f0c7de8c047ff7bed173ae961ab5899f3d15551)
+$(call add-radio-file-sha1-checked,radio/laf.img,2698631ae65f89e289f5f2f67acf721ba75c5871)
+$(call add-radio-file-sha1-checked,radio/modem.img,cf6026dc1412e62d1347a8d1f0dc78d14ab420cb)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,554c2f2ea73586c5aa659e4c7201a2f4f8bc0ac5)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,3d142a213dd66661a104a20a8537b8f4576f0bd9)
+$(call add-radio-file-sha1-checked,radio/tz.img,6b994af7839d34dd2e38fdb1c951caf233a4002d)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,1d84fe3ebee3ec275b7b95c2fac347d409c68948)
+$(call add-radio-file-sha1-checked,radio/xbl.img,f721dd35deb362135c7d66664499aede07277006)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,c64080c9158b47124b0734b8d6bc34e8d8cd50e1)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,24 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    akmu \
+    aop \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    laf \
+    modem \
+    multiimgoem \
+    qupfw \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
